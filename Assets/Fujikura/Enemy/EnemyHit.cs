af16734b0d88140f526ffa7ca20cb2deb97303cc
hakua_fujikura <126577771+hakunan@users.noreply.github.com>
2024-08-06 17:20:13 +0900

    EnemyHit修正

--- Assets/Fujikura/Enemy/EnemyHit.cs
+++ Assets/Fujikura/Enemy/EnemyHit.cs
@@ -1,16 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyHit : MonoBehaviour
 {
     [SerializeField] float amountReduced = 1;
+    [SerializeField] bool sky = false;
+
+    [SerializeField] Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+    }
 
     private void OnCollisionEnter(Collision other)
     {
-        if(other.gameObject.tag == "Player")
+        if(other.gameObject.CompareTag("Player"))
         {
-            StaminaBar.instance.StaminaDown(amountReduced);
+            HitPlayer();
+        }
+    }
+    void HitPlayer()
+    {
+        StaminaBar.instance.StaminaDown(amountReduced);
+        
+        if(sky)
+        {
+            rb.gravityScale = -100;
+            Destroy(gameObject, 3f);
+
+        }
+        else
+        {
+            rb.gravityScale = 10;
+            Destroy(gameObject, 3f);
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            HitPlayer();
         }
     }
 }
